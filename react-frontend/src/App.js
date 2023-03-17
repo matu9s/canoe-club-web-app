844c6768ed8263ad3cbb683b40a0e27567ef464a
Matúš Hluch <matus.hluch@gmail.com>
2023-03-17 21:57:17 +0100

    Add bootstrap to frontend and reformat

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,25 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}]);
   useEffect(() => {
-    fetch("/home").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-      }
-    )
-  }, [])
+    fetch("/home")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
 
   return (
-    <div>
-      {(typeof data.text === 'undefined') ? (
-        <p>waiting</p>) :
-        (<p>{data.text}</p>)}
-    </div>
+    <>
+      {typeof data.text === "undefined" ? <p>waiting</p> : <p>{data.text}</p>}
+    </>
   );
-
 }
 
 export default App;
