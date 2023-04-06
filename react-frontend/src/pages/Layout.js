f64fde552ebae9f7c131f2f930dfd3c8ba22ef25
Matúš Hluch <matus.hluch@gmail.com>
2023-04-06 21:44:07 +0200

    Add logout to frontend

--- react-frontend/src/pages/Layout.js
+++ react-frontend/src/pages/Layout.js
@@ -1,12 +1,30 @@
 import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { userContext } from "../UserContext";
+import Button from "react-bootstrap/Button";
 
 const Layout = () => {
   const { currentUser, setCurrentUser } = useContext(userContext);
+  const logout = () => {
+    setCurrentUser({});
+    localStorage.removeItem("current_user");
+    fetch("/api/logout/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
   return (
     <>
       <p>
+        {currentUser.username ? (
+          <Button variant="secondary" onClick={logout}>
+            Log out
+          </Button>
+        ) : (
+          ""
+        )}
         ToDo:Navbar Hello{" "}
         {currentUser.username ? currentUser.username : "Unknown"}
       </p>
