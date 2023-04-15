64d5507735daae9ef1c5ac940ca9f39d29ddedf7
Matúš Hluch <matus.hluch@gmail.com>
2023-04-15 14:58:03 +0200

    Add navbar

---
+++ react-frontend/src/components/AppNavBar.js
@@ -0,0 +1,73 @@
+import { useContext } from "react";
+import { userContext } from "../UserContext";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+
+const AppNavBar = () => {
+  const { currentUser, setCurrentUser } = useContext(userContext);
+  const navigate = useNavigate();
+  const logout = () => {
+    setCurrentUser({});
+    localStorage.removeItem("current_user");
+    fetch("/api/logout/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    navigate("/");
+  };
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Kayak/Canoe Administration</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {currentUser.username ? (
+              <>
+                <Nav.Link href="/">Home</Nav.Link>
+                <NavDropdown title="Boats" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/boat-list">
+                    Boats List
+                  </NavDropdown.Item>
+                  {
+                    <NavDropdown.Item href="/add-boat">
+                      Add Boat
+                    </NavDropdown.Item>
+                  }
+                </NavDropdown>
+                <Nav.Link href="/member-list">Members</Nav.Link>
+                <NavDropdown title="Trainings">
+                  <NavDropdown.Item href="/training-list">
+                    Trainings List
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/add-training">
+                    Add Training
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Navbar.Text>
+                  Signed in as: <b>{currentUser.username}</b>
+                </Navbar.Text>
+                <Button variant="secondary" onClick={logout}>
+                  Log out
+                </Button>
+              </>
+            ) : (
+              <>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/register">Register</Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default AppNavBar;
--- react-frontend/src/pages/Layout.js
+++ react-frontend/src/pages/Layout.js
@@ -1,33 +1,13 @@
 import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { userContext } from "../UserContext";
-import Button from "react-bootstrap/Button";
+import AppNavBar from "../components/AppNavBar";
 
 const Layout = () => {
   const { currentUser, setCurrentUser } = useContext(userContext);
-  const logout = () => {
-    setCurrentUser({});
-    localStorage.removeItem("current_user");
-    fetch("/api/logout/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  };
   return (
     <>
-      <p>
-        {currentUser.username ? (
-          <Button variant="secondary" onClick={logout}>
-            Log out
-          </Button>
-        ) : (
-          ""
-        )}
-        ToDo:Navbar Hello{" "}
-        {currentUser.username ? currentUser.username : "Unknown"}
-      </p>
+      <AppNavBar></AppNavBar>
       <Outlet />
     </>
   );
